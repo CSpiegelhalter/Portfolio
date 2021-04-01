b22b65bb9c6675c152a050459580f75c7e48a507
Curt Spiegelhalter <i16dcurt@gmail.com>
2021-04-01 12:49:52 -0500

    Ran deploy

---
+++ build/precache-manifest.0abf770f101ed865123b1d9d8590c5b9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ebb8686e0912d9787c106bf0264be0b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "790d566e3dc72cea18d3",
+    "url": "/Portfolio/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "58d84b754efa66fb65d2",
+    "url": "/Portfolio/static/css/main.3409aabc.chunk.css"
+  },
+  {
+    "revision": "790d566e3dc72cea18d3",
+    "url": "/Portfolio/static/js/2.d5bff551.chunk.js"
+  },
+  {
+    "revision": "c7b68bb99dd69105905090efe691b1ee",
+    "url": "/Portfolio/static/js/2.d5bff551.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58d84b754efa66fb65d2",
+    "url": "/Portfolio/static/js/main.27915746.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "ae0d1eb1c0d44864f102f1c59a61c56a",
+    "url": "/Portfolio/static/media/Bootstrap.ae0d1eb1.png"
+  },
+  {
+    "revision": "60c2c3c6bcf9b6c00feb63b7bc07e48d",
+    "url": "/Portfolio/static/media/CSS.60c2c3c6.png"
+  },
+  {
+    "revision": "237d79f502ced6eb168a806dbe26bd65",
+    "url": "/Portfolio/static/media/JsIcon.237d79f5.png"
+  },
+  {
+    "revision": "29cd51152bb279e742f7ac7d4ceb78f4",
+    "url": "/Portfolio/static/media/budget.29cd5115.png"
+  },
+  {
+    "revision": "f5e362028b8db4e6a87c71574cef454e",
+    "url": "/Portfolio/static/media/donde.f5e36202.png"
+  },
+  {
+    "revision": "931de62ba421511fa56f5d3034a801c9",
+    "url": "/Portfolio/static/media/fitness.931de62b.png"
+  },
+  {
+    "revision": "3b360d916b80603f34d477f177ec9fc8",
+    "url": "/Portfolio/static/media/htmlIcon.3b360d91.png"
+  },
+  {
+    "revision": "0d2c88f7c39ae5c1e0d05ac9a214f15e",
+    "url": "/Portfolio/static/media/mongo.0d2c88f7.png"
+  },
+  {
+    "revision": "c2c796d32ad70c520811ba1f2d5a007b",
+    "url": "/Portfolio/static/media/mySqlIcon.c2c796d3.png"
+  },
+  {
+    "revision": "683bb51f99495e6df8aabe3277bff19e",
+    "url": "/Portfolio/static/media/node.683bb51f.png"
+  },
+  {
+    "revision": "384c95a33127ca59d6e2ab3322c46d8d",
+    "url": "/Portfolio/static/media/notes.384c95a3.png"
+  },
+  {
+    "revision": "87620910e9a90a491d1a51bfe042b06b",
+    "url": "/Portfolio/static/media/python.87620910.png"
+  },
+  {
+    "revision": "5b1d823def8bc474671f4ee7a53798c2",
+    "url": "/Portfolio/static/media/reactIcon.5b1d823d.png"
+  }
+]);
